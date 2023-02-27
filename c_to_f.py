6248d0bfc8abee74d050b258bd91bbabaa07d5bd
NFNYLawrence <re08980970@gmail.com>
2023-02-27 23:19:03 +0800

    Add new file

---
+++ c_to_f.py
@@ -0,0 +1,11 @@
+# 攝氏('c)轉換成華氏('f)程式
+# 讓使用者輸入攝氏溫度, 然後印出華氏溫度
+temp_c = input('請輸入攝氏溫度: ')
+temp_c = float(temp_c)
+temp_c = temp_c * 9 / 5 + 32
+print(temp_c)
+# 以下為老師解答
+# temp_c = input('請輸入攝氏溫度: ')
+# temp_c = float(temp_c)
+# f = temp_c * 9 / 5 + 32
+# print('華氏溫度為: ', f)
